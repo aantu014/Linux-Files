99c7331664ea623a9ed916ad5b7ae4a78675be5f
aantu014 <50641330+aantu014@users.noreply.github.com>
2020-12-05 22:19:28 -0500

    Create Gentoo_part2.sh

---
+++ Gentoo/Gentoo_part2.sh
@@ -0,0 +1,52 @@
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+
+emerge-webrsync -q
+emerge --sync -q
+emerge -q --update --deep --newuse @world
+emerge -q app-editors/vim
+
+#Pick zone replce time zone with yours
+#ls /usr/share/zoneinfo
+#ls /usr/share/zoneinfo/America/
+echo "America/New_York" > /etc/timezone
+emerge --config sys-libs/timezone-data
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+eselect locale set 4
+
+emerge -q --autounmask-continue sys-kernel/gentoo-sources sys-kernel/genkernel
+
+
+emerge -q --autounmask-continue sys-kernel/gentoo-sources sys-kernel/genkernel
+
+#Configure kernel but look for tutorials.
+cd /usr/src/linux
+make menuconfig
+make && make modules_install && make install
+genkernel --install --kernel-config=/usr/src/linux/.config initramfs
+
+echo "Gentoo" > /etc/conf.d/hostname
+
+#Get your network interfaces.
+# ifconfig | grep "flag"
+
+echo 'config_enp0s3="dhcp"' > /etc/conf.d/net
+
+emerge -q net-misc/dhcpcd
+
+cd /etc/init.d
+ln -s net.lo net.enp0s3
+rc-update add net.enp0s3 default
+
+
+cd /
+emerge -q sys-boot/grub:2
+grub-install --target=x86_64-efi --efi-directory=/boot
+
+wget https://raw.githubusercontent.com/aantu014/Linux-Files/master/Gentoo/genfstab
+chmod +x genfstab
+./genfstab -p -U . >> /etc/fstab
+
+emerge -q app-admin/sudo
