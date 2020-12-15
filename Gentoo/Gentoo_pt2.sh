775630c782955b95628653e3b1a04adf5ac94afc
aantu014 <50641330+aantu014@users.noreply.github.com>
2020-12-15 01:01:57 -0500

    Update Gentoo_pt2.sh

--- Gentoo/Gentoo_pt2.sh
+++ Gentoo/Gentoo_pt2.sh
@@ -59,7 +59,8 @@ chmod +x genfstab
 
 emerge -q app-admin/sudo
 emerge -q x11-drivers/xf86-input-libinput
-emerge -q media-sound/alsa-utils
+emerge -q media-sound/alsa-util
+emerge -q x11-misc/xclip
 emerge -q net-misc/networkmanager
 
 for x in /etc/runlevels/default/net.* ; do rc-update del $(basename $x) default ; rc-service --ifstarted $(basename $x) stop; done
