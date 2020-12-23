8ddd26fbd5d9bc9ce022da6d24bcd511f4fb4cd3
aantu014 <50641330+aantu014@users.noreply.github.com>
2020-12-23 09:54:02 -0500

    Add files via upload

---
+++ Debian/Kali_pt1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#cfdisk /dev/sda
+#You can run   sed -i 's/sda/sdc/g' Kali_pt1.sh to replce with disk you want to install on. Replace sdc witht the device you want.
+
+#Device                         Start                End            Sectors          Size Type
+
+#/dev/sda1                       2048            1026047            1024000          500M EFI System                  
+#/dev/sda2                    1026048            3123199            2097152            4G Linux swap
+#/dev/sda3                    3123200          104857566          101734367           15G Linux filesystem
+#/dev/sda4                    3123200          104857566          101734367           15G Linux filesystem
+#/dev/sda5                    3123200          104857566          101734367            4G Linux filesystem
+#/dev/sda6                    3123200          104857566          101734367            1G Linux filesystem
+#/dev/sda7                    3123200          104857566          101734367          250G Linux filesystem
+
+
+mkfs.vfat -F32 -n EFIBOOT /dev/sda1
+mkswap /dev/sda2
+swapon /dev/sda2
+mkfs.ext4 -L kaliroot /dev/sda3
+mkfs.ext4 -L kaliusr /dev/sda4
+mkfs.ext4 -L kalivar /dev/sda5
+mkfs.ext4 -L kalitmp /dev/sda6
+mkfs.ext4 -L kalihome /dev/sda7
+
+mkdir /mnt/kali/
+
+mount /dev/sda3 /mnt/kali/
+mkdir /mnt/kali/usr
+mkdir /mnt/kali/var
+mkdir /mnt/kali/tmp
+mkdir /mnt/kali/home
+mkdir /mnt/kali/boot
+
+mount /dev/sda4 /mnt/kali/usr
+mount /dev/sda5 /mnt/kali/var
+mount /dev/sda6 /mnt/kali/tmp
+mount /dev/sda7 /mnt/kali/home
+mount /dev/sda1 /mnt/kali/boot
+
+
+debootstrap --variant=minbase --arch amd64 kali-rolling /mnt/kali http://http.kali.org/kali
+
+mount -t proc /proc /mnt/kali/proc
+mount --rbind --make-rslave /dev /mnt/kali/dev
+mount --rbind --make-rslave /sys /mnt/kali/sys
+mount --rbind --make-rslave /run /mnt/kali/run
+
+genfstab -U -p /mnt/kali >> /mnt/kali/etc/fstab
+
+chroot /mnt/kali /bin/bash
+
+
---
+++ Debian/Kali_pt2.sh
@@ -0,0 +1,69 @@
+echo "Kali.localdomain" > /etc/hostname
+cat <<EOF> /etc/hosts
+127.0.0.1	 localhost
+::1		 localhost
+127.0.1.1	 Kali.localdomain		Kali
+EOF
+
+cat <<EOF> /etc/apt/sources.list
+deb http://http.kali.org/kali kali-rolling main contrib non-free
+EOF
+
+
+apt update
+
+apt update && apt upgrade --no-install-recommends
+
+apt install --no-install-recommends debian-keyring vim systemd
+
+apt install --no-install-recommends locales && dpkg-reconfigure locales
+
+dpkg-reconfigure tzdata
+
+apt install --no-install-recommends linux-image-amd64
+
+vim /etc/mkinitcpio.conf
+
+###HOOKS=(base systemd autodetect modconf block keyboard sd-vconsole filesystems shutdown)###
+###Exeucte mkinitcpio
+mkinitcpio -p linux-lts
+###systemd-boot install:
+bootctl install
+###Edit the loader.conf file in the /boot/loader directory:
+cat <<EOF> /boot/loader/loader.conf
+timeout 3
+#console-mode keep
+default arch-*
+editor no
+EOF
+###Copy the arch.conf file to the  entries directory:
+
+cp /usr/share/systemd/bootctl/arch.conf /boot/loader/entries/
+###Edit the details for the arch.conf file:
+cat <<EOF> /boot/loader/entries/arch.conf
+title Kali Linux
+linux /vmlinuz-linux-lts
+initrd /initramfs-linux-lts.img
+options root=LABEL=kaliroot rw
+EOF
+###Update bootloader
+
+bootctl update
+
+###Root password:
+passwd
+
+echo "Enter new user name:"
+read user
+useradd -mG sudo $user
+passwd $user
+
+apt install --no-install-recommends xserver-xorg-input-libinput xinit xserver-xorg-core firefox-esr neofetch git alsa-utils sxiv nitrogen stterm dwm sudo
+
+echo "Uncomment '%sudo' in sudo. Press enter to continue."
+read continue
+
+vim /etc/sudoers
+
+echo "exec dwm" > ~/.xinitrc
+echo "startx" >> ~/.bash_profile
